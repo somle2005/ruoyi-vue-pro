f5272a4ee8ac0eca2597ff2fd2f967f67d5955f5
东宇 <wangdongyu@somle.com>
2024-12-31 10:24:30 +0800

    fix: 添加同步库存定时任务。

---
+++ somle-esb/src/main/java/com/somle/esb/job/EccangProductInventoryDataJob.java
@@ -0,0 +1,26 @@
+package com.somle.esb.job;
+
+import com.somle.framework.common.util.json.JSONObject;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *  eccang的getProductInventory，获取所有库存信息，存入本地数据库。
+ *  实现断点续传。1次/小时?
+ */
+public class EccangProductInventoryDataJob extends EccangDataJob {
+    @Override
+    public String execute(String param) throws Exception {
+        setDate(param);
+        AtomicInteger syncCount = new AtomicInteger();
+        AtomicInteger totalCount = new AtomicInteger();
+        eccangService.getAllPageWithResume(new JSONObject(), "getProductInventory")
+            .forEach(
+                eccangPage -> {
+                    syncCount.addAndGet(eccangPage.getPageSize());
+                    totalCount.set(eccangPage.getTotalCount());
+                }
+            );
+        return "同步完成，进度" + syncCount + "/" + totalCount;
+    }
+}
