4705866342b5ec99bf1e732892c0bbd1ca0da5fe
fishSteak <992484215@qq.com>
2025-01-04 11:08:34 +0800

    fix 使用record类简化代码

--- somle-framework/somle-common/src/main/java/com/somle/framework/common/util/io/SomleResponse.java
+++ somle-framework/somle-common/src/main/java/com/somle/framework/common/util/io/SomleResponse.java
@@ -1,24 +1,22 @@
 package com.somle.framework.common.util.io;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-@Data
-public class SomleResponse<T> {
-    private final int code;
-    private final T bodyData;
-    private final Map<String, List<String>> headers; // 加入响应头部信息
-
+/**
+ * @param headers 加入响应头部信息
+ */
+public record SomleResponse<T>(int code, T bodyData, Map<String, List<String>> headers) {
     public SomleResponse(int code, T bodyData, Map<String, List<String>> headers) {
         this.code = code;
         this.bodyData = bodyData;
         this.headers = headers != null ? headers : Collections.emptyMap();  // 确保 headers 非 null
     }
+
     @Getter
     @AllArgsConstructor
     public enum ResponseType {
