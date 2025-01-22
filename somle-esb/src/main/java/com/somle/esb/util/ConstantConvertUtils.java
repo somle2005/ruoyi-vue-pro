8717cec8e3646d04aa85e5601142a7a7404b630b
东宇 <wangdongyu@somle.com>
2025-01-22 10:14:03 +0800

    fix mm->cm，优化、修改

--- somle-esb/src/main/java/com/somle/esb/util/ConstantConvertUtils.java
+++ somle-esb/src/main/java/com/somle/esb/util/ConstantConvertUtils.java
@@ -32,7 +32,7 @@ public final class ConstantConvertUtils {
 
     // 辅助方法：转换为厘米并返回Float，避免重复代码
     public static Float mmConvertToCm(Integer mmValue) {
-        return mmValue != null ? mmValue / 10f : null;
+        return mmValue != null ? mmValue / 10F : null;
     }
     public static Float mmConvertToCm(Float mmValue) {
         return mmValue != null ? mmValue / 10F : null;
