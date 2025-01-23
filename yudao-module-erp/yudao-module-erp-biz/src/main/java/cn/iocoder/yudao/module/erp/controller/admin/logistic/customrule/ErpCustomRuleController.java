6bbfd8332f7ca18147c30a75ca7984d0ae898c18
东宇 <wangdongyu@somle.com>
2025-01-23 16:13:26 +0800

    fix 修改

--- yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/controller/admin/logistic/customrule/ErpCustomRuleController.java
+++ yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/controller/admin/logistic/customrule/ErpCustomRuleController.java
@@ -1,38 +1,37 @@
 package cn.iocoder.yudao.module.erp.controller.admin.logistic.customrule;
 
 import cn.hutool.core.collection.CollUtil;
-import cn.iocoder.yudao.framework.common.util.collection.MapUtils;
-import cn.iocoder.yudao.module.erp.api.product.ErpCustomRuleApi;
-import cn.iocoder.yudao.module.erp.api.product.dto.ErpCustomRuleDTO;
-import cn.iocoder.yudao.module.erp.controller.admin.product.vo.product.ErpProductRespVO;
-import cn.iocoder.yudao.module.erp.service.product.ErpProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import jakarta.annotation.Resource;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.security.access.prepost.PreAuthorize;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Operation;
-
-import jakarta.validation.*;
-import jakarta.servlet.http.*;
-import java.util.*;
-import java.io.IOException;
-
+import cn.iocoder.yudao.framework.apilog.core.annotation.ApiAccessLog;
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
-import cn.iocoder.yudao.framework.common.pojo.CommonResult;
+import cn.iocoder.yudao.framework.common.util.collection.MapUtils;
 import cn.iocoder.yudao.framework.common.util.object.BeanUtils;
-import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 import cn.iocoder.yudao.framework.excel.core.util.ExcelUtils;
-import cn.iocoder.yudao.framework.apilog.core.annotation.ApiAccessLog;
-import static cn.iocoder.yudao.framework.apilog.core.enums.OperateTypeEnum.*;
-import static cn.iocoder.yudao.framework.common.util.collection.CollectionUtils.convertSet;
-
-import cn.iocoder.yudao.module.erp.controller.admin.logistic.customrule.vo.*;
+import cn.iocoder.yudao.module.erp.controller.admin.logistic.customrule.vo.ErpCustomRulePageReqVO;
+import cn.iocoder.yudao.module.erp.controller.admin.logistic.customrule.vo.ErpCustomRuleRespVO;
+import cn.iocoder.yudao.module.erp.controller.admin.logistic.customrule.vo.ErpCustomRuleSaveReqVO;
+import cn.iocoder.yudao.module.erp.controller.admin.product.vo.product.ErpProductRespVO;
 import cn.iocoder.yudao.module.erp.dal.dataobject.logistic.customrule.ErpCustomRuleDO;
 import cn.iocoder.yudao.module.erp.service.logistic.customrule.ErpCustomRuleService;
+import cn.iocoder.yudao.module.erp.service.product.ErpProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static cn.iocoder.yudao.framework.apilog.core.enums.OperateTypeEnum.EXPORT;
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
+import static cn.iocoder.yudao.framework.common.util.collection.CollectionUtils.convertSet;
 
 @Tag(name = "管理后台 - ERP 海关规则")
 @RestController
@@ -91,22 +90,22 @@ public class ErpCustomRuleController {
     @PreAuthorize("@ss.hasPermission('erp:custom-rule:export')")
     @ApiAccessLog(operateType = EXPORT)
     public void exportCustomRuleExcel(@Valid ErpCustomRulePageReqVO pageReqVO,
-              HttpServletResponse response) throws IOException {
+                                      HttpServletResponse response) throws IOException {
         pageReqVO.setPageSize(PageParam.PAGE_SIZE_NONE);
         List<ErpCustomRuleRespVO> list = bindPageResult(customRuleService.getCustomRulePage(pageReqVO)).getList();
         // 导出 Excel
         ExcelUtils.write(response, "ERP 海关规则.xls", "数据", ErpCustomRuleRespVO.class, list);
     }
 
-    private PageResult<ErpCustomRuleRespVO> bindPageResult(PageResult<ErpCustomRuleDO> pageResult){
+    private PageResult<ErpCustomRuleRespVO> bindPageResult(PageResult<ErpCustomRuleDO> pageResult) {
         if (CollUtil.isEmpty(pageResult.getList())) {
             return PageResult.empty(pageResult.getTotal());
         }
         //1.1 产品信息
         Map<Long, ErpProductRespVO> productVOMap = erpProductService.getProductVOMap(convertSet(pageResult.getList(), ErpCustomRuleDO::getProductId));
         //2开始拼接
-        return BeanUtils.toBean(pageResult,ErpCustomRuleRespVO.class, erpCustomRule -> {
-            MapUtils.findAndThen(productVOMap,erpCustomRule.getProductId(), erpCustomRule::setProduct);//设置产品VO实体类
+        return BeanUtils.toBean(pageResult, ErpCustomRuleRespVO.class, erpCustomRule -> {
+            MapUtils.findAndThen(productVOMap, erpCustomRule.getProductId(), erpCustomRule::setProduct);//设置产品VO实体类
 
         });
     }
