64d758205637a70f665608173ac5ea7863ab36cf
wqh <2242313915@qq.com>
2025-02-06 16:25:48 +0800

    update(LeeFJ)   shopfy 接口优化

--- somle-shopify/src/test/java/com/somle/shopify/service/ShopifyServiceTest.java
+++ somle-shopify/src/test/java/com/somle/shopify/service/ShopifyServiceTest.java
@@ -1,6 +1,8 @@
 package com.somle.shopify.service;
 
 
+import com.somle.framework.common.util.json.JSONArray;
+import com.somle.framework.common.util.json.JSONObject;
 import com.somle.framework.test.core.ut.BaseDbUnitTest;
 import com.somle.framework.test.core.ut.BaseSpringTest;
 import com.somle.shopify.model.ShopifyToken;
@@ -9,14 +11,46 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Import;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @Slf4j
 @Import({ShopifyService.class})
 class ShopifyServiceTest extends BaseDbUnitTest {
+
     @Resource
     ShopifyService service;
 
     @Test
-    void test() {
-        log.info(service.client.getProducts().toString());
+    void testGetProducts() {
+        JSONArray products= service.client().getProducts();
+        assertNotNull(products);
+        log.info(products.toString());
+        assertFalse(products.isEmpty());
     }
+
+    @Test
+    void testGetShop() {
+        JSONObject shop= service.client().getShop();
+        assertNotNull(shop);
+        log.info(shop.toString());
+        assertNotNull(shop);
+        assertFalse(shop.isEmpty());
+    }
+
+    @Test
+    void testGetOrders() {
+        JSONArray orders= service.client().getOrders();
+        assertNotNull(orders);
+        log.info(orders.toString());
+        assertFalse(orders.isEmpty());
+    }
+
+    @Test
+    void testGetPayouts() {
+        JSONArray payouts= service.client().getPayouts();
+        assertNotNull(payouts);
+        log.info(payouts.toString());
+        assertFalse(payouts.isEmpty());
+    }
+
 }
