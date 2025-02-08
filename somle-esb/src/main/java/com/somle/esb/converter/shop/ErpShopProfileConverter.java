1bbccad0d39eed75a76a697362a36a5294b3a298
LeeFJ <leefangjie@qq.com>
2025-02-08 18:17:29 +0800

    update  增加模型转换器

---
+++ somle-esb/src/main/java/com/somle/esb/converter/shop/ErpShopProfileConverter.java
@@ -0,0 +1,63 @@
+package com.somle.esb.converter.shop;
+
+import com.somle.framework.domain.sales.SalesPlatform;
+import com.somle.framework.domain.shop.ShopProfileType;
+import com.somle.framework.domain.shop.model.ShopProfileDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+* @Author LeeFJ
+* @Description 销售平台数据模型转换工具
+* @Date 13:34 2025/2/7
+* @Param
+* @return
+**/
+public abstract class ErpShopProfileConverter<IN,OUT> {
+
+    private static final Map<String, ErpShopProfileConverter<?,?>> CONVERTERS = new HashMap<>();
+
+    /**
+    * @Author LFJ
+    * @Description TODO
+    * @Date 9:28 2025/2/8
+    * @Param [shopInfoDTO]
+    * @return VO
+    **/
+
+    public static <VO> VO convert(ShopProfileDTO shopInfoDTO) {
+        String key = makeKey(shopInfoDTO.getSalesPlatform(),shopInfoDTO.getShopProfileType());
+        ErpShopProfileConverter<?,?> converter= CONVERTERS.get(key);
+        if (converter==null) {
+            throw new IllegalArgumentException("未找到对应的转换器:"+shopInfoDTO.getSalesPlatform().name()+"."+shopInfoDTO.getShopProfileType().name());
+        }
+        return (VO)converter.toModel(shopInfoDTO);
+    }
+
+
+    protected ErpShopProfileConverter(SalesPlatform salesPlatform, ShopProfileType shopInfoType) {
+        CONVERTERS.put(makeKey(salesPlatform, shopInfoType),this);
+    }
+
+    private static String makeKey(SalesPlatform salesPlatform, ShopProfileType shopInfoType) {
+        return salesPlatform.name()+"_"+shopInfoType.name();
+    }
+
+    protected OUT toModel(ShopProfileDTO<IN> shopInfoDTO) {
+        if (shopInfoDTO == null) {
+            return null;
+        }
+        return toModelInternal(shopInfoDTO);
+    }
+
+    protected abstract OUT toModelInternal(ShopProfileDTO<IN> shopInfoDTO);
+
+}
+
+
+
+
+
+
+
