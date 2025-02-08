8cd8228cbd344783836eb49626a8efb45844cc81
wqh <2242313915@qq.com>
2025-02-08 15:45:45 +0800

    update(LeeFJ) 销售平台定义

---
+++ somle-framework/somle-common/src/main/java/com/somle/framework/domain/sales/SalesPlatform.java
@@ -0,0 +1,28 @@
+package com.somle.framework.domain.sales;
+
+import lombok.Getter;
+
+@Getter
+public enum SalesPlatform {
+
+    AMAZON(false,null),
+    JINGDONG(false,null),
+    SHOPIFY(true,"https://fitueyes.com"),
+    BESTBUY(false,null),
+    MANOMANO(false,null),
+    MATOMO(false,null),
+    OTTO(false,null),
+    RAKUTEN(false,null),
+    WALMART(false,null),
+    WANGDIAN(false,null),
+    WAYFAIR(false,null);
+
+    private String siteURL;
+    private boolean isSyncProfile;
+
+    private SalesPlatform(boolean isSyncProfile,String siteURL) {
+        this.isSyncProfile=isSyncProfile;
+        this.siteURL=siteURL;
+    }
+
+}
