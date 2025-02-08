240c9f1ea9091d9265daaccc0d27b0dea2bda18b
LeeFJ <leefangjie@qq.com>
2025-02-08 18:41:02 +0800

    update 解决代码冲突

--- somle-esb/src/test/java/com/somle/esb/job/SyncErpProductJobTest.java
+++ somle-esb/src/test/java/com/somle/esb/job/SyncErpProductJobTest.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
+import com.somle.framework.domain.SomleConsts;
 
 @Slf4j
 public class SyncErpProductJobTest extends BaseSpringIntegrationTest {
