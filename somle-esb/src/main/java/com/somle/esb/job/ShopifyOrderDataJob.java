06e76f74fd76a887088e9bd23748baeec00f9e9d
wqh <2242313915@qq.com>
2025-02-08 15:49:28 +0800

    update(LeeFJ) 调整方法报文返回形式，使其与数据分析侧兼容

--- somle-esb/src/main/java/com/somle/esb/job/ShopifyOrderDataJob.java
+++ somle-esb/src/main/java/com/somle/esb/job/ShopifyOrderDataJob.java
@@ -1,13 +1,9 @@
 package com.somle.esb.job;
 
 
-import com.somle.amazon.controller.vo.AmazonSpOrderReqVO;
 import com.somle.esb.model.OssData;
-import com.somle.framework.common.util.json.JSONObject;
-import com.somle.framework.common.util.json.JsonUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 @Component
 public class ShopifyOrderDataJob extends ShopifyDataJob {
@@ -17,7 +13,7 @@ public class ShopifyOrderDataJob extends ShopifyDataJob {
     public String execute(String param) throws Exception {
         setDate(param);
 
-        var result = shopifyService.client().getOrders();
+        var result = shopifyClient.getRawOrders();
         var data = OssData.builder()
                 .database(DATABASE)
                 .tableName("order")
