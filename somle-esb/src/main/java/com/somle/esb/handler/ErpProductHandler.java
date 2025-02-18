e16937ab38e2f3d7283b7f219fbd2c22566be699
东宇 <wangdongyu@somle.com>
2025-02-18 20:11:39 +0800

    BUG 对于默认金额币种设置CNY字符串，而不是code。

--- somle-esb/src/main/java/com/somle/esb/handler/ErpProductHandler.java
+++ somle-esb/src/main/java/com/somle/esb/handler/ErpProductHandler.java
@@ -10,7 +10,6 @@ import com.somle.kingdee.model.KingdeeProductSaveReqVO;
 import com.somle.kingdee.service.KingdeeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -24,7 +23,7 @@ import java.util.List;
  */
 @Slf4j
 @Component
-@Profile("!dev & !test")
+//@Profile("prod")
 @RequiredArgsConstructor
 public class ErpProductHandler {
 
