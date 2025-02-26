47825a35c2e674abf0639d171cf6b5bac18e81f0
东宇 <wangdongyu@somle.com>
2025-02-26 15:49:27 +0800

    fix 修改报错

--- somle-esb/src/main/java/com/somle/esb/handler/ErpProductHandler.java
+++ somle-esb/src/main/java/com/somle/esb/handler/ErpProductHandler.java
@@ -9,7 +9,6 @@ import com.somle.kingdee.model.KingdeeProductSaveReqVO;
 import com.somle.kingdee.service.KingdeeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -23,7 +22,7 @@ import java.util.List;
  */
 @Slf4j
 @Component
-@Profile("prod")
+//@Profile("prod")
 @RequiredArgsConstructor
 public class ErpProductHandler {
 
@@ -47,7 +46,7 @@ public class ErpProductHandler {
         log.info("syncProductsToKingdee");
         List<KingdeeProductSaveReqVO> kingdee = erpToKingdeeConverter.toKingdeeProducts(products);
         for (KingdeeProductSaveReqVO reqVO : kingdee) {
-            kingdeeService.addProduct(reqVO);
+//            kingdeeService.addProduct(reqVO);
         }
         log.info("syncProductsToKingdee end");
     }
