b6f9f514737535f65c41139b98b181c3185dfcc7
gumaomao <474317126@qq.com>
2025-02-28 09:07:24 +0800

    feature:Cdiscount第三方平台对接，获取订单相关信息

---
+++ somle-cdiscount/src/main/java/com/somle/cdiscount/service/CdiscountClient.java
@@ -0,0 +1,80 @@
+package com.somle.cdiscount.service;
+
+
+
+import cn.iocoder.yudao.framework.common.util.http.HttpUtils;
+import cn.iocoder.yudao.framework.common.util.json.JSONObject;
+import cn.iocoder.yudao.framework.common.util.json.JsonUtilsX;
+import cn.iocoder.yudao.framework.common.util.web.RequestX;
+import cn.iocoder.yudao.framework.common.util.web.WebUtils;
+import com.somle.cdiscount.model.CdiscountToken;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+public class CdiscountClient {
+
+    private final CdiscountToken token;
+    //Cdiscount平台API请求路径
+    private final String URL = "https://api.octopia-io.net/seller/v2";
+
+    private final String TOKEN_URL = "https://auth.octopia-io.net/auth/realms/maas/protocol/openid-connect/token";
+
+   //    此参数拼接方式必须配合header中"Content-Type", "application/x-www-form-urlencoded"方式使用
+    private final String TOKEN_REQUEST_BODY = "client_id=gumaomao&client_secret=D6zTxDeCXErst2Kvr7D4AYrJI0ZMiIdP&grant_type=client_credentials";
+
+    @Setter
+    private OkHttpClient webClient;
+
+   //卖家ID
+    public static final String SELLER_ID = "79730";
+
+    public CdiscountClient(CdiscountToken token) {
+        this.token = token;
+        this.webClient = new OkHttpClient();
+    }
+
+    public Map<String, String> getHeaders() throws IOException {
+        return Map.of(
+            "Content-Type", "application/json",
+            "sellerId", SELLER_ID,
+            "Authorization", "Bearer " + getAccessToken()
+        );
+    }
+
+    @SneakyThrows
+    public JSONObject getOrders() {
+       //Cdiscount平台订单查询接口路径
+        var endpoint = "/orders?salesChannelId=CDISFR&shippingCountry=Fr";
+        var request = RequestX.builder()
+            .requestMethod(RequestX.Method.GET)
+            .url(URL+endpoint)
+            .headers(getHeaders())
+            .build();
+        var bodyString = sendRequest(request).body().string();
+        var result = JsonUtilsX.parseObject(bodyString, JSONObject.class);
+        return result;
+    }
+
+    @SneakyThrows
+    private Response sendRequest(RequestX request) {
+        // Define the proxy details
+        return webClient.newCall(WebUtils.toOkHttp(request)).execute();
+    }
+
+    public String getAccessToken(){
+
+        String body = HttpUtils.post(TOKEN_URL, Map.of(
+                        "Content-Type", "application/x-www-form-urlencoded"
+                ), TOKEN_REQUEST_BODY
+        );
+        CdiscountToken token = JsonUtilsX.parseObject(body, CdiscountToken.class);
+        return token.getAccess_token();
+    }
+
+}
