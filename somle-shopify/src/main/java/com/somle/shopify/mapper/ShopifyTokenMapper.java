ea3724ce33c14426546bf639e5417c3cd818910c
zxh <zhouxinhang@somle.com>
2025-03-18 14:12:30 +0800

    feat:oms模块基础代码

--- somle-shopify/src/main/java/com/somle/shopify/mapper/ShopifyTokenMapper.java
+++ somle-shopify/src/main/java/com/somle/shopify/mapper/ShopifyTokenMapper.java
@@ -2,8 +2,11 @@ package com.somle.shopify.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.somle.shopify.domain.ShopifyToken;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface ShopifyTokenMapper extends BaseMapper<ShopifyToken> {
+
+    String getHeaders(@Param("storeName") String storeName);
+
 }
--- somle-shopify/src/main/java/com/somle/shopify/service/ShopifyClient.java
+++ somle-shopify/src/main/java/com/somle/shopify/service/ShopifyClient.java
@@ -1,6 +1,8 @@
 package com.somle.shopify.service;
 
 
+import com.somle.shopify.mapper.ShopifyTokenMapper;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class ShopifyClient {
+
+    @Resource
+    private ShopifyTokenMapper shopifyTokenMapper;
+
+    public String getHeaders(String storeName) {
+        String accessToken = shopifyTokenMapper.getHeaders(storeName);
+        return accessToken;
+    }
 
 
 }
