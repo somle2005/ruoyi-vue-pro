3b2a365d22ee0d8fb4d822c0f4e1a3679d867093
zxh <zhouxinhang@somle.com>
2025-03-24 09:39:48 +0800

    refactor:删除shopify的token

--- somle-shopify/src/main/java/com/somle/shopify/service/ShopifyService.java
+++ somle-shopify/src/main/java/com/somle/shopify/service/ShopifyService.java
@@ -78,7 +78,6 @@ public class ShopifyService {
         // 拿shopName 调用平台接口 目前 shopify token为固定
         String accessToken = null;
         if ("Shopify_FIT_USA".equals(shopAndTokenInfo.getShopName())) {
-            accessToken = "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";
         }
         shopAndTokenInfo.setAccessToken(accessToken);
         return shopAndTokenInfo;
