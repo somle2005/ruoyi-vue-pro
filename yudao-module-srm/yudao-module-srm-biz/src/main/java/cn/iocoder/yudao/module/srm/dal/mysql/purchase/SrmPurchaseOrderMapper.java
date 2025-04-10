86d796fe795787e4213d56aeda44d70dc5b3328e
东宇 <wangdongyu@somle.com>
2025-04-10 20:21:03 +0800

    fix 订单子表范围的主表返回需要优化。需要分页主表	主表单独查 + 子表用 IN 批量查

--- yudao-module-srm/yudao-module-srm-biz/src/main/java/cn/iocoder/yudao/module/srm/dal/mysql/purchase/SrmPurchaseOrderMapper.java
+++ yudao-module-srm/yudao-module-srm-biz/src/main/java/cn/iocoder/yudao/module/srm/dal/mysql/purchase/SrmPurchaseOrderMapper.java
@@ -1,5 +1,6 @@
 package cn.iocoder.yudao.module.srm.dal.mysql.purchase;
 
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
 import cn.iocoder.yudao.framework.mybatis.core.query.MPJLambdaWrapperX;
 import cn.iocoder.yudao.module.srm.controller.admin.purchase.vo.order.req.SrmPurchaseOrderPageReqVO;
@@ -56,9 +57,9 @@ public interface SrmPurchaseOrderMapper extends BaseMapperX<SrmPurchaseOrderDO> 
     }
 
     //需要分页主表	主表单独查 + 子表用 IN 批量查
-//    default PageResult<SrmPurchaseOrderDO> selectPage(SrmPurchaseOrderPageReqVO reqVO) {
-//        return selectPage(reqVO, getBOWrapper(reqVO));
-//    }
+    default PageResult<SrmPurchaseOrderDO> selectPage(SrmPurchaseOrderPageReqVO reqVO) {
+        return selectPage(reqVO, wrapper(reqVO));
+    }
 
     default int updateByIdAndStatus(Long id, Integer status, SrmPurchaseOrderDO updateObj) {
         return update(updateObj,
