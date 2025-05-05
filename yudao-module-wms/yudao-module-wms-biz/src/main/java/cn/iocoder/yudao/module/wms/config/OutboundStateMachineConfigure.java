58b12aef84e35968b19e0ce789616491461f5b3d
LeeFJ <leefangjie@qq.com>
2025-05-05 15:58:39 +0800

    update 增加导出接口

--- yudao-module-wms/yudao-module-wms-biz/src/main/java/cn/iocoder/yudao/module/wms/config/OutboundStateMachineConfigure.java
+++ yudao-module-wms/yudao-module-wms-biz/src/main/java/cn/iocoder/yudao/module/wms/config/OutboundStateMachineConfigure.java
@@ -7,7 +7,7 @@ import cn.iocoder.yudao.framework.cola.statemachine.builder.TransitionContext;
 import cn.iocoder.yudao.module.wms.dal.dataobject.outbound.WmsOutboundDO;
 import cn.iocoder.yudao.module.wms.enums.outbound.WmsOutboundAuditStatus;
 import cn.iocoder.yudao.module.wms.service.outbound.transition.OutboundAgreeTransitionHandler;
-import cn.iocoder.yudao.module.wms.service.outbound.transition.OutboundExecuteTransitionHandler;
+import cn.iocoder.yudao.module.wms.service.outbound.transition.OutboundFinishTransitionHandler;
 import cn.iocoder.yudao.module.wms.service.outbound.transition.OutboundRejectTransitionHandler;
 import cn.iocoder.yudao.module.wms.service.outbound.transition.OutboundSubmitTransitionHandler;
 import cn.iocoder.yudao.module.wms.service.outbound.transition.OutboundTransitionFailCallback;
@@ -65,7 +65,7 @@ public class OutboundStateMachineConfigure {
             .from(WmsOutboundAuditStatus.PASS.getValue())
             .to(WmsOutboundAuditStatus.FINISHED.getValue())
             .on(WmsOutboundAuditStatus.Event.FINISH)
-            .handle(OutboundExecuteTransitionHandler.class);
+            .handle(OutboundFinishTransitionHandler.class);
 
         // 失败处理
         builder.setFailCallback(OutboundTransitionFailCallback.class);
--- yudao-module-wms/yudao-module-wms-biz/src/main/java/cn/iocoder/yudao/module/wms/service/outbound/transition/OutboundFinishTransitionHandler.java
+++ yudao-module-wms/yudao-module-wms-biz/src/main/java/cn/iocoder/yudao/module/wms/service/outbound/transition/OutboundFinishTransitionHandler.java
@@ -2,6 +2,7 @@ package cn.iocoder.yudao.module.wms.service.outbound.transition;
 
 
 import cn.iocoder.yudao.framework.cola.statemachine.builder.TransitionContext;
+import cn.iocoder.yudao.module.system.enums.somle.BillType;
 import cn.iocoder.yudao.module.wms.dal.dataobject.outbound.WmsOutboundDO;
 import cn.iocoder.yudao.module.wms.enums.outbound.WmsOutboundAuditStatus;
 import cn.iocoder.yudao.module.wms.service.quantity.OutboundFinishExecutor;
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Component;
  * @description: 执行出库
  */
 @Component
-public class OutboundExecuteTransitionHandler extends BaseOutboundTransitionHandler {
+public class OutboundFinishTransitionHandler extends BaseOutboundTransitionHandler {
 
     @Resource
     private OutboundFinishExecutor outboundFinishExecutor;
@@ -29,5 +30,14 @@ public class OutboundExecuteTransitionHandler extends BaseOutboundTransitionHand
         OutboundContext outboundContext = new OutboundContext();
         outboundContext.setOutboundId(context.data().getId());
         outboundFinishExecutor.execute(outboundContext);
+
+
+        //TODO 判断单据来源生成目标仓库的入库单
+        BillType billType = BillType.parse(context.data().getUpstreamBillType());
+        // 如果时
+        if(billType==null) {
+
+        }
+
     }
 }
