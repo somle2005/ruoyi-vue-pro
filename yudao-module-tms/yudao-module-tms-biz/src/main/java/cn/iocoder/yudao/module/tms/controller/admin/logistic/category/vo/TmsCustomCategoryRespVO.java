b832120ce9ad1cc5927633a657fd9ef59a4d3752
东宇 <wangdongyu@somle.com>
2025-05-07 17:16:32 +0800

    feat(tms): 为报关分类维护添加用途描述和材质描述字段
    - 在 TmsCustomCategoryDO 中添加 customsPurpose 和 customsMaterial 字段
    - 更新 TmsCustomCategoryMapper 以支持新字段的查询
    - 在 TmsCustomCategoryPageReqVO 和 TmsCustomCategorySaveReqVO 中添加新字段的请求参数
    - 在 TmsCustomCategoryRespVO 中添加新字段的响应数据

--- yudao-module-tms/yudao-module-tms-biz/src/main/java/cn/iocoder/yudao/module/tms/controller/admin/logistic/category/vo/TmsCustomCategoryRespVO.java
+++ yudao-module-tms/yudao-module-tms-biz/src/main/java/cn/iocoder/yudao/module/tms/controller/admin/logistic/category/vo/TmsCustomCategoryRespVO.java
@@ -33,6 +33,14 @@ public class TmsCustomCategoryRespVO {
     @ExcelProperty("英文品名")
     private String declaredTypeEn;
 
+    @Schema(description = "用途描述", requiredMode = Schema.RequiredMode.REQUIRED)
+    @ExcelProperty("用途描述")
+    private String customsPurpose;
+
+    @Schema(description = "报关材质描述", requiredMode = Schema.RequiredMode.REQUIRED)
+    @ExcelProperty("报关材质描述")
+    private String customsMaterial;
+
     @Schema(description = "材质对应string+报关品名")
     @ExcelProperty("材质对应string+报关品名")
     private String combinedValue;
