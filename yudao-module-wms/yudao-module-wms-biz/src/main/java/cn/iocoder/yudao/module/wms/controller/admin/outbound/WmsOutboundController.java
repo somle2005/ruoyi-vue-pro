a2867682d7354149d7c1b97fdd33194d751f94a9
jisencai <jisencai@163.com>
2025-05-27 13:49:40 +0800

    fix(wms): 出库单人员名单补充

--- yudao-module-wms/yudao-module-wms-biz/src/main/java/cn/iocoder/yudao/module/wms/controller/admin/outbound/WmsOutboundController.java
+++ yudao-module-wms/yudao-module-wms-biz/src/main/java/cn/iocoder/yudao/module/wms/controller/admin/outbound/WmsOutboundController.java
@@ -101,6 +101,11 @@ public class WmsOutboundController {
         WmsOutboundRespVO outboundVO = outboundService.getOutboundWithItemList(id);
         // 装配出库仓位
         outboundItemService.assembleBin(outboundVO.getItemList());
+        // 人员姓名填充
+        AdminUserApi.inst().prepareFill(List.of(outboundVO))
+                .mapping(WmsOutboundRespVO::getCreator, WmsOutboundRespVO::setCreatorName)
+                .mapping(WmsOutboundRespVO::getUpdater, WmsOutboundRespVO::setUpdaterName)
+                .fill();
         // 返回
         return success(outboundVO);
     }
