10c027fe2b1e82264b807ae83e8620592b7ca002
东宇 <wangdongyu@somle.com>
2025-05-28 20:37:46 +0800

    feat(tms): 调拨单出库审核后创建入库单
    - 在 TmsTransferApiImpl 类中添加创建入库单的逻辑
    - 新增与 WmsInboundApi 的交互，调用 createInbound 方法创建入库单
    - 将调拨单信息转换为入库单信息，包括主单和明细项
    - 记录创建入库单的日志信息

--- yudao-module-tms/yudao-module-tms-biz/src/main/java/cn/iocoder/yudao/module/tms/api/transfer/TmsTransferApiImpl.java
+++ yudao-module-tms/yudao-module-tms-biz/src/main/java/cn/iocoder/yudao/module/tms/api/transfer/TmsTransferApiImpl.java
@@ -7,20 +7,28 @@ import cn.iocoder.yudao.module.tms.api.transfer.dto.TmsOutboundReqDTO;
 import cn.iocoder.yudao.module.tms.dal.dataobject.transfer.TmsTransferDO;
 import cn.iocoder.yudao.module.tms.dal.dataobject.transfer.item.TmsTransferItemDO;
 import cn.iocoder.yudao.module.tms.dal.mysql.transfer.TmsTransferMapper;
+import cn.iocoder.yudao.module.tms.service.bo.transfer.TmsTransferBO;
 import cn.iocoder.yudao.module.tms.service.transfer.TmsTransferService;
 import cn.iocoder.yudao.module.tms.service.transfer.item.TmsTransferItemService;
+import cn.iocoder.yudao.module.wms.api.inbound.WmsInboundApi;
+import cn.iocoder.yudao.module.wms.api.inbound.dto.WmsInboundItemSaveReqDTO;
+import cn.iocoder.yudao.module.wms.api.inbound.dto.WmsInboundSaveReqDTO;
+import cn.iocoder.yudao.module.wms.enums.inbound.WmsInboundType;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 调拨单 API 实现类
  */
 @Service
+@Slf4j
 public class TmsTransferApiImpl implements TmsTransferApi {
 
     @Resource
@@ -31,6 +39,9 @@ public class TmsTransferApiImpl implements TmsTransferApi {
 
     @Resource
     private TmsTransferItemService transferItemService;
+
+    @Resource
+    private WmsInboundApi wmsInboundApi;
 
     @Override
     @Transactional(rollbackFor = Exception.class)
@@ -53,5 +64,33 @@ public class TmsTransferApiImpl implements TmsTransferApi {
         transfer.setOutboundId(reqDTO.getId());
         transfer.setOutboundCode(reqDTO.getCode());
         transferMapper.updateById(transfer);
+
+        //4.0 创建对应入库单
+        TmsTransferBO transferBO = transferService.getTransferBO(reqDTO.getId());
+        //仓库to ,
+        Long inbound = wmsInboundApi.createInbound(
+            WmsInboundSaveReqDTO.builder()
+                .type(WmsInboundType.TRANSFER.getValue())
+                .upstreamBillType(BillType.TMS_TRANSFER.getValue())
+                .upstreamBillId(transferBO.getId())
+                .upstreamBillCode(transferBO.getCode())
+                .warehouseId(transferBO.getToWarehouseId())
+                //TODO 主单是否有库存公司
+//                .companyId(transferBO.getCompanyId())
+//                .deptId(transferBO.getDeptId())
+                .arrivalPlanTime(reqDTO.getOutboundTime())
+                .itemList(transferBO.getTmsTransferItemDOList().stream()
+                    .map(item -> WmsInboundItemSaveReqDTO.builder()
+                        .productId(item.getProductId())
+                        .planQty(item.getQty())
+//                        .deptId(item.getDeptId())
+                        .companyId(item.getStockCompanyId())
+                        .remark(item.getRemark())
+                        .upstreamItemId(item.getId())
+                        .build())
+                    .collect(Collectors.toList()))
+                .build()
+        );
+        log.info("调拨单[{}]出库审核通过，创建入库单，ID: {}", transferBO.getCode(), inbound);
     }
 } 
