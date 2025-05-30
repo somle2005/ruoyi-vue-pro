2e83e7aa09ff4d935f9dbbfb5358a36be936f014
东宇 <wangdongyu@somle.com>
2025-05-30 14:01:15 +0800

    feat(tms): 添加港口名称重复校验
    - 在创建和更新港口信息时增加名称重复校验逻辑
    - 新增 PORT_INFO_NAME_DUPLICATE 错误码用于处理港口名称重复的情况
    - 优化代码结构，提高可维护性

--- yudao-module-tms/yudao-module-tms-biz/src/main/java/cn/iocoder/yudao/module/tms/service/port/info/TmsPortInfoServiceImpl.java
+++ yudao-module-tms/yudao-module-tms-biz/src/main/java/cn/iocoder/yudao/module/tms/service/port/info/TmsPortInfoServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import java.util.List;
 
 import static cn.iocoder.yudao.framework.common.exception.util.ServiceExceptionUtil.exception;
+import static cn.iocoder.yudao.module.tms.enums.TmsErrorCodeConstants.PORT_INFO_NAME_DUPLICATE;
 import static cn.iocoder.yudao.module.tms.enums.TmsErrorCodeConstants.PORT_INFO_NOT_EXISTS;
 
 /**
@@ -29,10 +30,11 @@ public class TmsPortInfoServiceImpl implements TmsPortInfoService {
 
     @Override
     public Integer createPortInfo(TmsPortInfoSaveReqVO createReqVO) {
+        // 校验港口名称重复
+        validatePortInfoNameDuplicate(null, createReqVO.getName());
         // 插入
         TmsPortInfoDO portInfo = BeanUtils.toBean(createReqVO, TmsPortInfoDO.class);
         portInfoMapper.insert(portInfo);
-        //校验港口name 重复
         // 返回
         return portInfo.getId();
     }
@@ -41,7 +43,8 @@ public class TmsPortInfoServiceImpl implements TmsPortInfoService {
     public void updatePortInfo(TmsPortInfoSaveReqVO updateReqVO) {
         // 校验存在
         validatePortInfoExists(updateReqVO.getId());
-        //校验港口name 重复
+        // 校验港口名称重复
+        validatePortInfoNameDuplicate(updateReqVO.getId(), updateReqVO.getName());
         // 更新
         TmsPortInfoDO updateObj = BeanUtils.toBean(updateReqVO, TmsPortInfoDO.class);
         portInfoMapper.updateById(updateObj);
@@ -58,6 +61,13 @@ public class TmsPortInfoServiceImpl implements TmsPortInfoService {
     private void validatePortInfoExists(Integer id) {
         if (portInfoMapper.selectById(id) == null) {
             throw exception(PORT_INFO_NOT_EXISTS);
+        }
+    }
+
+    private void validatePortInfoNameDuplicate(Integer id, String name) {
+        TmsPortInfoDO portInfo = portInfoMapper.selectOne(TmsPortInfoDO::getName, name);
+        if (portInfo != null && !portInfo.getId().equals(id)) {
+            throw exception(PORT_INFO_NAME_DUPLICATE, name);
         }
     }
 
