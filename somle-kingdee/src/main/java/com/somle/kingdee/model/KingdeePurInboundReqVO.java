0b86f9e89ce3505cefa1915edab55e4a0137b355
东宇 <wangdongyu@somle.com>
2025-06-05 17:48:54 +0800

    refactor(somle): 重构采购入库单请求对象初始化方式- 将 KingdeePurInboundReqVO 的初始化方式从使用 Lombok 的 Builder 改为直接使用 new 关键字 -移除了 KingdeePurInboundReqVO 类中的 @Builder 注解

--- somle-kingdee/src/main/java/com/somle/kingdee/model/KingdeePurInboundReqVO.java
+++ somle-kingdee/src/main/java/com/somle/kingdee/model/KingdeePurInboundReqVO.java
@@ -3,7 +3,6 @@ package com.somle.kingdee.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.Builder;
 import lombok.Data;
 
 import java.sql.Timestamp;
@@ -13,7 +12,7 @@ import java.util.List;
 /**
  * 采购入库单列表查询请求对象
  */
-@Builder
+//@Builder
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KingdeePurInboundReqVO {
