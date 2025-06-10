1a60abcc79d4e82faafd7f6424a6a6d847bf4610
jisencai <jisencai@163.com>
2025-06-10 10:35:47 +0800

    fix(wms): 移除多余方法，增加WmsOutboundImportReqDTO非空校验

--- yudao-module-wms/yudao-module-wms-api/src/main/java/cn/iocoder/yudao/module/wms/api/outbound/WmsOutboundApi.java
+++ yudao-module-wms/yudao-module-wms-api/src/main/java/cn/iocoder/yudao/module/wms/api/outbound/WmsOutboundApi.java
@@ -2,7 +2,6 @@ package cn.iocoder.yudao.module.wms.api.outbound;
 
 import cn.iocoder.yudao.module.wms.api.outbound.dto.WmsOutboundDTO;
 import cn.iocoder.yudao.module.wms.api.outbound.dto.WmsOutboundImportReqDTO;
-import cn.iocoder.yudao.module.wms.api.outbound.dto.WmsOutboundSaveReqDTO;
 
 import java.util.List;
 
@@ -12,22 +11,6 @@ import java.util.List;
  * @description: 出库单 API
  */
 public interface WmsOutboundApi {
-
-    /**
-     * 创建出库单
-     *
-     * @param createReqDTO 出库单创建请求
-     * @return 出库单ID
-     */
-    Long createOutbound(WmsOutboundSaveReqDTO createReqDTO);
-
-    /**
-     * 按 ID 查询出库单
-     *
-     * @param id 出库单更新请求
-     * @return WmsInboundDTO
-     */
-    WmsOutboundDTO getOutbound(Long id);
 
     /**
      * 按 上游单据类型 和 上游单据ID 查询出库单
@@ -46,9 +29,9 @@ public interface WmsOutboundApi {
     void abandonOutbound(Long id,String comment);
 
     /**
-     * 根据入库单生成出库单
+     * 生成出库单
+     *
      * @param importReqVO 入参
-     * @return 出参
      */
-    WmsOutboundDTO generateOutbound(WmsOutboundImportReqDTO importReqVO);
+    void generateOutbound(WmsOutboundImportReqDTO importReqVO);
 }
